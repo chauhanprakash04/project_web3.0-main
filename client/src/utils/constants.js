2553d303e757fee9f1406bc72a4869d17d03e0a8
Prakash <plc7209@gmail.com>
2023-04-08 17:55:41 +0530

    final

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xf1c8b5953028D95C9892a1D94DC49dD9F97bFdd3'
+export const contractAddress = '0xc533c83415BF394DC3Df9E74d5527c8fA335591c'
